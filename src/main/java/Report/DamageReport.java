a2fbb6cd693bb4c66a67cf74ed7238bac492cdae
azaa-x <n.andrean119@gmail.com>
2025-05-28 20:33:13 +0700

    update package

--- src/main/java/Report/DamageReport.java
+++ src/main/java/Report/DamageReport.java
@@ -1,5 +1,44 @@
 package Report;
 
-public class DamageReport {
+import java.util.Date;
 
+public class DamageReport extends Report {
+    private String tingkatKerusakan;
+    private String statusPerbaikan;
+
+    public DamageReport(String deskripsi, Date tanggalLaporan, String tingkatKerusakan) {
+        super(deskripsi, tanggalLaporan);
+        this.tingkatKerusakan = tingkatKerusakan;
+        this.statusPerbaikan = "Belum diperbaiki";
+    }
+
+    public String getTingkatKerusakan() {
+        return tingkatKerusakan;
+    }
+
+    public void setTingkatKerusakan(String tingkatKerusakan) {
+        this.tingkatKerusakan = tingkatKerusakan;
+    }
+
+    public String getStatusPerbaikan() {
+        return statusPerbaikan;
+    }
+
+    public void setStatusPerbaikan(String statusPerbaikan) {
+        this.statusPerbaikan = statusPerbaikan;
+    }
+
+    public void ubahStatusPerbaikan(String statusBaru) {
+        this.statusPerbaikan = statusBaru;
+    }
+
+    @Override
+    public void tampilkanLaporan() {
+        System.out.println("===== Laporan Kerusakan =====");
+        System.out.println("Deskripsi        : " + getDeskripsi());
+        System.out.println("Tanggal          : " + getTanggalLaporan());
+        System.out.println("Tingkat Kerusakan: " + tingkatKerusakan);
+        System.out.println("Status Perbaikan : " + statusPerbaikan);
+        System.out.println();
+    }
 }
